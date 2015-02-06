ad6b4ecff8f6b1c933c2b4751c8550d8a72f147b
wangzhenqing <wangzhenqing1008@163.com>
2015-02-06 18:20:59 +0800

    增加删除

--- src/main/java/com/wzq/security/controller/UserController.java
+++ src/main/java/com/wzq/security/controller/UserController.java
@@ -5,6 +5,7 @@ import com.wzq.security.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -27,5 +28,17 @@ public class UserController {
         List<User> userList = userService.getAllUserList();
         mav.addObject("userList", userList);
         return mav;
+    }
+
+    /**
+     * 删除值
+     *
+     * @param id
+     * @return
+     */
+    @RequestMapping(value = "/delete")
+    public String delete(@RequestParam("id") String id) {
+        userService.delUserById(Integer.valueOf(id));
+        return "redirect:/users";
     }
 }
